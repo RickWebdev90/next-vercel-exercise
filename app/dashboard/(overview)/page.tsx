3e29e9b1f34823dd3634e80f34149c6ebaec2551
Rick Lochner <ricklochner@MacBook-Air.fritz.box>
2024-02-13 07:48:13 +0100

    finish app

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,11 @@ import { fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Dashboard | Acme Dashboard"
+  }
  
 export default async function Page() {
   
